19285822cf4412e480ba86f54ad32c7e9c2d3bb9
dengzhuangfeng <dengzhuangfeng@gmail.com>
2020-04-03 14:10:24 +0800

    update

--- gamespreview/dotslinkcrush/main.c38ee.js
+++ gamespreview/dotslinkcrush/main.c38ee.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.242d3.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.1e075.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
