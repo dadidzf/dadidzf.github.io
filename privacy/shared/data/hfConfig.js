0b824d424df397c65af0f4e24fecd81c68e32761
dadidzf <dengzhuangfeng@gmail.com>
2022-09-29 21:51:34 +0800

    update

--- privacy/shared/data/hfConfig.js
+++ privacy/shared/data/hfConfig.js
@@ -1,12 +1,17 @@
 var herofight = {
     name: "长沙英雄用武科技有限公司",
-    email: "this property will complate by Old Deng"
+    email: "464874358@qq.com"
 };
 
 var witsplay = {
     name: "长沙市慧玩网络科技有限公司",
-    email: "this property will complate by Old Deng"
+    email: "herofight@foxmail.com"
 };
+
+var swordfish = {
+    name: "成都箭鱼科技有限公司",
+    email: "1611471@qq.com"
+}
 
 var maketime = "2017年9月8日";
 
@@ -15,9 +20,11 @@ var updatetime = "2022年9月30日";
 var hfConfig = (function () {
     var hf = herofight;
     var wp = witsplay;
+    var sf = swordfish;
     return {
         hf: hf,
         wp: wp,
+        sf: sf,
         mt: maketime,
         ut: updatetime
     }
